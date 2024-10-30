163074151807f8767d09a8d49beadbcd75dd8ade
laribox <laribox2014@gmail.com>
2024-10-30 00:04:12 +0000

    task 3 answer

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,28 +13,30 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ Initialize the cache """
         super().__init__()
-        self.stack = []
+        self.lru_order = []
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """ Add an item in the cache """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            self.stack.remove(key)
+            self.lru_order.remove(key)
         elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            last_key = self.stack.pop()
-            del self.cache_data[last_key]
-            print("DISCARD:", last_key)
+
+            least_recently_used = self.lru_order.pop(0)
+            del self.cache_data[least_recently_used]
+            print("DISCARD:", least_recently_used)
 
         self.cache_data[key] = item
-        self.stack.append(key)
+        self.lru_order.append(key)
 
     def get(self, key):
-        """ Get an item by key
-        """
+        """ Get an item by key """
         if key is None or key not in self.cache_data:
-            return
-        else:
-            return self.cache_data[key]
+            return None
+
+        self.lru_order.remove(key)
+        self.lru_order.append(key)
+        return self.cache_data[key]
+
