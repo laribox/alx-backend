5a7704d34286bfc1b62a6bae695702e0fe3c25ba
laribox <laribox2014@gmail.com>
2024-11-05 21:09:06 +0000

    task 2

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,7 +26,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """
-    get the local parameters 
+    get the local parameters
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
