f9db356e05044df5ce658379a6e5a7c9c62fb507
laribox <laribox2014@gmail.com>
2024-11-05 20:13:28 +0000

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+0. Basic Flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """basic route """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
